8778bc5bde51b9bf5182473825ebec32e6d6b6d6
barisenak <mariya1999-7@mail.ru>
2020-11-05 00:53:21 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Component } from "react";
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Square label="This is a big square" cssClass="bigSquare" />
+        <Square label="This is a middle square" cssClass="middleSquare" />
+        <Square label="This is a little square" cssClass="miniSquare" />
+      </div>
+    );
+  }
+}
+
+class Square extends Component {
+  render() {
+    return <div className={this.props.cssClass}>{this.props.label}</div>;
+  }
+}
 export default App;
